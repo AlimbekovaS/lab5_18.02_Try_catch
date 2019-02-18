16033b53a7e083d16384360b28747ee89ff9c906
АлимбековаС <@201_10>
2019-02-18 18:09:13 +0600

    Add project files.

---
+++ lab5_Try_catch/LogTableOverflowExeption.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_Try_catch
+{
+    public class LogTableOverflowExeption
+        : Exception
+    {
+        const string overflowMessage = "The log table has overflowed";
+        public LogTableOverflowExeption(string auxMessage, Exception inner)
+            : base(string.Format("{0} - {1}", overflowMessage, auxMessage), inner)
+        {
+            this.HelpLink = "http://google.com";
+            this.Source = "Exception_Class_Samples" +
+                "";
+
+        }
+    }
+
+    public class LogTable
+        {
+        protected string[] logArea;
+        protected int elemntUse;
+        public LogTable(int numElements)
+        {
+            logArea = new string[numElements];
+            elemntUse = 0;
+        }
+
+        public int AddRecord(string newRecord)
+        {
+            try
+            {
+                logArea[elemntUse] = newRecord;
+                return elemntUse++;
+
+            }
+            catch (Exception e)
+            {
+                throw new LogTableOverflowExeption(string.Format("Record \"{0}\" was not logged", newRecord), e);
+                throw;
+            }
+        }
+        }
+}
---
+++ lab5_Try_catch/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_Try_catch
+{
+    class Program
+    {
+        static int[] numbers = { 2, 4, 5, 6, 8, 9 };
+        public static int GetNumber(int index)
+        {
+            if(index<0||index>=numbers.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return numbers[index];
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                GetNumber(66);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+          
+
+           
+
+            int x = 1;
+            int y = 0;
+            try
+            {
+                int result = x / y;
+            }
+            catch (Exception ex) when(y==0)
+            {
+
+                Console.WriteLine("у не должно быть равно 0");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            LogTable log = new LogTable(4);
+            Console.WriteLine("---");
+            try
+            {
+                for (int count = 1; ; count++)
+                {
+                    log.AddRecord(string.Format("Log record number {0}", count));
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("\nMessage---\n{0}", ex.Message);
+                Console.WriteLine("\n Help Link---\n{0}", ex.HelpLink);
+                Console.WriteLine("\n Sourse---\n{0}", ex.Source);
+                Console.WriteLine("\n StackTrace---\n{0}", ex.StackTrace);
+                Console.WriteLine("\n TargetSite---\n{0}", ex.TargetSite);
+            }
+
+        }
+    }
+}
